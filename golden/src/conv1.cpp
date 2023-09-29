696e64b4699a851c080aec44a94c4288946c5a86
bevans <81346899+bevans-o@users.noreply.github.com>
2023-09-29 20:33:31 +1000

    image padding (conv1)

--- golden/src/conv1.cpp
+++ golden/src/conv1.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+int clamp(int value, int min, int max);
+
 // implements conv1 layer of SRCNN
 void conv1(ftmap_t input_ftmap[N0][H][W],
            param_t conv1_weights[N1][N0][F1][F1],
@@ -10,31 +12,32 @@ void conv1(ftmap_t input_ftmap[N0][H][W],
            ftmap_t output_ftmap[N1][H][W])
 {
 	// implement conv1 layer of SRCNN here
-		//Calculate padding
-		int startPaddingH = floor((F1 - H%F1) / 2);
-		int endPaddingH = ceil((F1 - H%F1) / 2);
-		int startPaddingW = floor((F1 - W%F1) / 2);
-		int endPaddingW = ceil((F1 - W%F1) / 2);
+		int padding = (F1 - 1) / 2;
 
-	    //apply N1 convolutions
+	    // apply N1 convolutions
 	    for (int n1 = 0; n1 < N1; n1++) {
-	        //each convolution has an output feature map of size H x W
-			//how much we increment W/H depends on the stride - I cannot see where this is specified for this layer, so have assumed 1
-	        for (int h = -startPaddingH; h < H+endPaddingH; h++) {
-	            for (int w = -startPaddingW; w < W+endPaddingW; w++) {
+
+	        // each convolution has an output feature map of size H x W
+			// how much we increment W/H depends on the stride - I cannot see where this is specified for this layer, so have assumed 1
+	        for (int h = 0; h < H; h++) {
+	            for (int w = 0; w < W; w++) {
+
 	                //each kernel has a size of F1 x F1
 	                for (int f1h = 0; f1h < F1; f1h++) {
 	                    for (int f1w = 0; f1w < F1; f1w++) {
-	                        //input has N0 features
+
+							// check for overflow - if there is, clamp (i.e. extend edge values)
+							int yPixelClamped = clamp(h + f1h - padding, 0, H);
+							int xPixelClamped = clamp(w + f1w - padding, 0, W);
+							
+	                        // input has N0 features
 	                        for (int n0 = 0; n0 < N0; n0++) {
-								//if feature map is within range, add weight
-								if (h + f1h >=0 && h+f1h < H && w+f1w < W && w + f1w >=0) {
-									output_ftmap[n1][h][w] += conv1_weights[n1][n0][f1h][f1w] * input_ftmap[n0][h+f1h][w+f1w];
-								}
+								output_ftmap[n1][h][w] += conv1_weights[n1][n0][f1h][f1w] * input_ftmap[n0][yPixelClamped][xPixelClamped];
 	                        }
 	                    }
 	                }
-	                //add bias, and apply relu
+
+	                // add bias, and apply relu
 	                output_ftmap[n1][h][w] = output_ftmap[n1][h][w] + conv1_biases[n1];
 	                if (output_ftmap[n1][h][w] < 0) {
 	                    output_ftmap[n1][h][w] = 0;
@@ -43,4 +46,11 @@ void conv1(ftmap_t input_ftmap[N0][H][W],
 	        }
 	    }
 
+}
+
+
+int clamp(int value, int min, int max) {
+	if (value < min) return min;
+	if (value > max) return max;
+	return value;
 }
